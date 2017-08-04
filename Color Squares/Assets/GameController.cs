347516e29912702b048667ca1f2b14ef87340828
gullpacco <gullpacco@gmail.com>
2017-08-04 16:09:48 +0200

    Push

---
+++ Color Squares/Assets/GameController.cs
@@ -0,0 +1,516 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+
+
+    public Transform ground;
+    bool countDownState;
+    int firstTile, secondTile, thirdTile = -1, counter, toClick;
+    public int level=1;
+    int touchState = 0;
+    float countDownTimer=0;
+    public GameObject countDownPanel;
+    bool tutorialComplete = true;
+    bool next;
+
+    public int maxTiles = 2;
+    public int score=-1;
+    public static GameController instance;
+    public Text scoreText, tutorialText, loseText, causeText, levelText;
+    bool firstWhite, firstBlack, firstRed, firstGreen=false, firstBlue, firstComplexThree, firstLocked, firstTimeIncrease, locked, gameOver;
+    public bool tutorial, pause;
+    public AudioSource error, coin;
+    public int[] scoreLimits = new int[11] { -1, 4, 9, 14, 19, 24, 29, 34, 39, 100000, 10000000 };
+    float timeLimit=0, minimumTimeLimit=1.5f;
+    public Slider timeSlider;
+    public GameObject tutorialWindow, loseWindow;
+    Camera mainCamera;
+    SectionController[] sections;
+    string wrong = "Wrong Tap", timeUp = "Time Up";
+  
+    public int ToClick
+    {
+        get { return toClick; }
+        set { toClick =value; }
+    }
+
+   
+    public enum ColoursEnum
+    {
+        green=0, red=1, white=2, black=3, lightBlue=4
+    }
+  
+
+    void Awake()
+    {
+        mainCamera = FindObjectOfType<Camera>();
+        Screen.orientation = ScreenOrientation.Portrait;
+        instance = this;
+        sections = FindObjectsOfType<SectionController>();
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        int state = 0;
+        if (tutorial && !countDownState)
+        {
+            Time.timeScale = 0;
+            tutorialWindow.SetActive(true);
+        }
+
+
+        StartCoroutine(SwitchColour());
+        if (timeLimit == 0)
+            timeLimit = 2;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (gameOver) {
+                Restart();
+            }
+            else if (tutorial) { CloseTutorial(); }
+            else
+            {
+                //Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(t).position);
+                RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), -Vector2.zero);
+
+                if (hit)
+                {
+                    if (hit.transform.name.Contains("Terrain"))
+                    {
+                        //  CheckCorrect(hit);
+                        state = sections[0].CheckCorrect(hit);
+
+                        if (state == 1)
+                        {
+                            coin.Play();
+                            state = 0;
+                        }
+                        if (state != -1 && level > 7)
+                        {
+                            state = sections[1].CheckCorrect(hit);
+                            if (state == 1)
+
+                            {
+                                coin.Play();
+                                state = 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        if (Input.touchCount == 1)
+        {
+
+
+            if (Input.GetTouch(0).phase == TouchPhase.Began && touchState == 0)
+            {
+                if (gameOver)
+                {
+                    touchState = 1;
+                    Restart();
+
+                }
+                else if (tutorial)
+                {
+                    touchState = 1;
+                    CloseTutorial();
+                }
+                else
+                {
+                    RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position), -Vector2.zero);
+                    touchState = 1;
+                    if (hit)
+                    {
+
+
+                        if (hit.transform.name.Contains("Terrain"))
+                        {
+                            //CheckCorrect(hit);
+                            state = sections[0].CheckCorrect(hit);
+                            if (state == 1)
+                            {
+                                coin.Play();
+                                state = 0;
+                            }
+                            if (state != -1 && level > 7)
+                            {
+                                state = sections[1].CheckCorrect(hit);
+                                if (state == 1)
+
+                                {
+                                    coin.Play();
+                                    state = 0;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                touchState = 0;
+
+            }
+
+        }
+
+        timeSlider.value -= Time.deltaTime;
+
+        if (state == -1)
+        {
+            error.Play();
+            GameOver(wrong);
+        }
+       
+    }
+
+    
+    
+
+    public void CloseTutorial()
+    {
+      if (next)
+            {
+            level = 8;
+            Application.LoadLevel(1);
+            }
+        countDownState = true;
+
+        tutorialWindow.SetActive(false);
+        //tutorial = false;
+        countDownPanel.SetActive(true);
+        Resume();
+    }
+
+    void Resume()
+    {
+        Time.timeScale = 1;
+        //tutorial = false;
+        pause = false;
+    }
+
+
+  
+    IEnumerator SwitchColour()
+    {
+      
+        yield return new WaitForSeconds(timeLimit);
+
+        if (toClick > 0 && tutorialComplete) {
+            if (!countDownState)
+                GameOver(timeUp);
+        }
+
+        if (countDownState )
+        {
+            countDownPanel.SetActive(false);
+            countDownState = false;
+        }
+            timeSlider.maxValue = timeLimit;
+        timeSlider.value = timeLimit;
+        toClick = 0;
+        {
+
+            if (score == scoreLimits[1])
+            {
+                if (tutorial)
+                {
+                    sections[0].ManualGeneration(2, new int[2] { (int)ColoursEnum.green, (int)ColoursEnum.red }, (int)ColoursEnum.black);
+                    tutorial = false;
+                    tutorialComplete = true;
+
+                }
+                else if ( tutorialComplete)
+                {
+                    tutorialComplete = false;
+                    toClick = 1;
+                    tutorialWindow.SetActive(true);
+                    tutorialText.text = "With black background\n only tap \n the RED tiles";
+                    level = 2;
+                    levelText.text = "Level: " + score;
+
+                    tutorial = true;
+
+                }
+            }
+
+           else if (score == scoreLimits[2])
+            {
+                if (tutorial)
+                {
+                    sections[0].ManualGeneration(2, new int[2] { (int)ColoursEnum.white, (int)ColoursEnum.black }, (int)ColoursEnum.green);
+                    tutorial = false;
+                    tutorialComplete = true;
+
+                }
+                else if (tutorialComplete)
+                {
+                    tutorialComplete = false;
+                    toClick = 2;
+                    tutorialWindow.SetActive(true);
+                    tutorialText.text = "With green background\n tap ALL tiles";
+                    level = 3;
+                    levelText.text = "Level: " + score;
+                    tutorial = true;
+
+                }
+            }
+            else if (score == scoreLimits[3])
+            {
+                if (tutorial)
+                {
+                    sections[0].ManualGeneration(2, new int[2] { (int)ColoursEnum.black, (int)ColoursEnum.white }, (int)ColoursEnum.red);
+                    tutorial = false;
+                    tutorialComplete = true;
+
+                }
+                else if (tutorialComplete)
+                {
+                    tutorialComplete = false;
+                    toClick = 0;
+                    tutorialWindow.SetActive(true);
+                    tutorialText.text = "With red background \n Don't tap anything";
+                    level = 4;
+                    levelText.text = "Level: " + score;
+
+                    tutorial = true;
+
+                }
+            }
+            else if (score == scoreLimits[4])
+            {
+                if (tutorial)
+                {
+                    sections[0].ManualGeneration(3, new int[3] { (int)ColoursEnum.lightBlue, (int)ColoursEnum.white, (int)ColoursEnum.black }, (int)ColoursEnum.red);
+                    tutorial = false;
+                    tutorialComplete = true;
+
+                }
+                else if (tutorialComplete)
+                {
+                    tutorialComplete = false;
+                    toClick = 1;
+                    tutorialWindow.SetActive(true);
+                    tutorialText.text = "ALWAYS TAP\n blue tiles \n on ANY background";
+                    level = 5;
+                    levelText.text = "Level: " + score;
+
+                    tutorial = true;
+
+                }
+            }
+
+            //else if (score == scoreLimits[4])
+            //{
+            //    tutorial = true;
+            //    tutorialWindow.SetActive(true);
+            //    if (tutorialComplete)
+            //    {
+            //        sections[0].ManualGeneration(3, new int[3] { (int)ColoursEnum.lightBlue, (int)ColoursEnum.white, (int)ColoursEnum.black }, (int)ColoursEnum.red);
+            //        tutorialComplete = false;
+            //    }
+            //    toClick = 1;
+            //    level = 5;
+            //    levelText.text = "Level: " + level;
+            //    tutorialText.text = "ALWAYS TAP\n blue tiles \n on ANY background";
+            //    countDownState = true;
+
+            //}
+            else
+            {
+
+
+                if (sections.Length == 1)
+                {
+                    toClick += sections[0].SwitchColour(maxTiles);
+                }
+                else
+                {
+                    int splits = Random.Range(0, maxTiles + 1);
+                    for (int s = 0; s < sections.Length; s++)
+                    {
+                        toClick += sections[s].SwitchColour(splits);
+                        splits = maxTiles - splits;
+
+                    }
+                }
+
+            }
+            //  else
+
+     /*        if (score == scoreLimits[5])
+            {
+                if (tutorial)
+                {
+                    tutorial = false;
+                    tutorialComplete = true;
+
+                }
+                else if (tutorialComplete)
+                {
+                    tutorialComplete = false;
+                    tutorialWindow.SetActive(true);
+                    tutorialText.text = "Let's add\n one more tile";
+                    level = 6;
+                    levelText.text = "Level: " + score;
+
+                    tutorial = true;
+
+                }
+            }
+
+
+          /*  if (score == scoreLimits[5])
+            {
+                if (tutorial)
+                {
+                    tutorial = false;
+                    tutorialComplete = true;
+
+                }
+                else if (tutorialComplete)
+                {
+                    tutorialComplete = false;
+                    tutorialWindow.SetActive(true);
+                    tutorialText.text = "Going faster now";
+                    level = 6;
+                    timeLimit -= 0.1f;
+                    levelText.text = "Level: " + score;
+
+                    tutorial = true;
+
+                }
+            }*/
+            if (score == scoreLimits[5])
+            {
+                firstTimeIncrease = false;
+                tutorial = true;
+                level = 6;
+                tutorialWindow.SetActive(true);
+                levelText.text = "Level: " + level;
+                tutorialText.text = "Let's split the screen";
+                countDownState = true;
+
+                next = true;
+            }
+            //}
+            //if (firstLocked)
+            //        {
+            //            if (k == firstTile)
+            //            {
+            //                firstLocked = false;
+            //                tiles[k].Locked = true;
+            //                locked = true;
+            //                tutorial = true;
+            //                tutorialWindow.SetActive(true);
+            //                levelText.text = "Level: " + level;
+            //                tutorialText.text = "Tiles with a STAR \n (with correct colour) \n must be tapped TWICE \n ";
+            //            }
+            //        }
+
+            //    if (level > 6)
+            //    {
+            //        if (!locked)
+            //        {
+            //            locked = tiles[k].TryLock(Random.Range(0, 11));
+            //        }
+            //    }
+            //}
+
+
+            //}
+
+            if(tutorialComplete)
+            {
+                scoreText.text = ++score + "";
+
+                if (timeLimit > 1.5f)
+                {
+                    timeLimit -= 0.01f;
+                }
+            }
+
+
+            if (tutorial)
+            {
+                if (score == 0)
+                {
+                    sections[0].ManualGeneration(2, new int[2] { (int)ColoursEnum.green, (int)ColoursEnum.red }, (int)ColoursEnum.white);
+                    tutorial = false;
+                    tutorialComplete = true;
+
+                }
+            }
+            else  if (score == 0 && tutorialComplete)
+              {
+                firstWhite = true;
+                tutorialComplete = false;
+                toClick = 1;
+                tutorialWindow.SetActive(true);
+                tutorialText.text = "Tap the GREEN tiles";
+                level = 1;
+              
+                    tutorial = true;
+
+            }
+        }
+
+     
+        
+
+        StopAllCoroutines();
+        }
+    
+
+    void GameOver(string cause)
+    {
+        if (!gameOver)
+        {
+            loseWindow.SetActive(true);
+            causeText.text = cause;
+            loseText.text = "Your Score: " + score;
+            Time.timeScale = 0;
+            gameOver = true;
+        }
+    }
+
+
+
+
+  
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+   //void CountDown()
+   // {
+   //     if (countDownTimer > 0)
+   //     {
+   //         countDownText.text = "" + countDownTimer;
+   //         countDownTimer--;
+   //         Invoke("CountDown", 1);
+   //     }
+   //     else
+   //     { countDownState = false;
+   //         countDownPanel.SetActive(false);
+   //     }
+   // }
+
+}
+
+
+
---
+++ Color Squares/Assets/SectionController.cs
@@ -0,0 +1,428 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SectionController : MonoBehaviour {
+
+    Tile[] tiles;
+    Tile[] activeTiles = new Tile[3];
+    int counter = 0;
+    int toClick = 0;
+    int bgValue = 0;
+    int[] tilePositions;
+    public static Color[] colors = new Color[5] {
+                      new Color(0.25f, 0.8f,0.25f),
+         new Color (0.8f, 0.25f, 0.25f),
+             new Color(1,1,1),
+             new Color(0,0,0),
+            new Color(0.4f, 0.9f, 9f)};
+    SpriteRenderer bgCol;
+    class Tile
+    {
+        GameObject tile_reference, star;
+        int val = 0, tileNumber;
+        bool clicked, locked;
+        SpriteRenderer renderer, childRenderer;
+
+        public int Value
+        {
+            get { return val; }
+            set { val = value; }
+
+        }
+
+        public bool Clicked
+        {
+            get { return clicked; }
+            set { clicked = value; }
+        }
+
+        public GameObject Tile_Reference
+        {
+            get { return tile_reference; }
+        }
+
+        public int TileNumber
+        {
+            get { return tileNumber; }
+        }
+
+        public void SetColour(int bgValue)
+        {
+            renderer.color = colors[bgValue];
+            for (int k=0; k<colors.Length; k++)
+            {
+                if (k == val && val!=bgValue)
+                {
+                    renderer.transform.GetChild(2).GetChild(k).gameObject.SetActive(true);
+                }
+                else
+                {
+                    renderer.transform.GetChild(2).GetChild(k).gameObject.SetActive(false);
+                }
+            }
+        }
+
+        public Tile(GameObject go, int number)
+        {
+            tile_reference = go;
+            renderer = go.GetComponent<SpriteRenderer>();
+            childRenderer = go.transform.GetChild(0).GetComponent<SpriteRenderer>();
+            star = go.transform.GetChild(1).gameObject;
+            tileNumber = number;
+        }
+
+        public bool Locked
+        {
+            get { return locked; }
+            set
+            {
+                locked = value;
+                star.SetActive(locked);
+            }
+        }
+
+        public bool TryLock(float chance)
+        {
+            if (chance > 8)
+                Locked = true;
+            return locked;
+        }
+
+
+    }
+
+    void Awake()
+    {
+        bgCol = GetComponent<SpriteRenderer>();
+        tiles = new Tile[transform.childCount];
+        {
+            for (int p = 0; p < tiles.Length; p++)
+            {
+                tiles[p] = new Tile(transform.GetChild(p).gameObject, counter++);
+            }
+        }
+    }
+
+
+    // Use this for initialization
+
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bgCol.color = colors[ bgValue];
+	}
+
+    public int SwitchColour(int tileCount)
+    {
+        toClick = 0;
+        tilePositions = new int[tileCount];
+
+        GenerateBackground();
+        if (tileCount > 0)
+        {
+
+            ActivateTiles(tileCount);
+        }
+        SetTilesColour();
+
+
+
+        return toClick;
+    }
+
+    void ActivateTiles(int tileCount)
+    {
+
+        PickTilesPosition(tileCount);
+
+    }
+
+    void PickTilesPosition(int tileCount)
+    {
+        tilePositions[0] = Random.Range(0, tiles.Length);
+        if (tileCount > 1)
+        {
+            do { tilePositions[1] = Random.Range(0, tiles.Length); }
+            while (tilePositions[1] == tilePositions[0]);
+            if (tileCount > 2)
+            {
+                do { tilePositions[2] = Random.Range(0, tiles.Length); }
+                while (tilePositions[2] == tilePositions[1] || tilePositions[2] == tilePositions[0]);
+            }
+        }
+
+    }
+
+    void SetTilesColour()
+    {
+        for (int k = 0; k < tiles.Length; k++)
+        {
+            bool found = false;
+            tiles[k].Clicked = false;
+            tiles[k].Locked = false;
+
+            for (int t = 0; t < tilePositions.Length; t++)
+            {
+                if (tilePositions[t] == k)
+                {
+                    tiles[k].Value = GenerateTile();
+                    found = true;
+
+
+                }
+            }
+            if (!found) tiles[k].Value = bgValue;
+            tiles[k].SetColour(bgValue);
+
+        }
+    }
+
+    int GenerateTile()
+    {
+        int tileVal;
+        if (GameController.instance.level <= 5)
+        {
+            tileVal = ColourFromBackground(false);
+        }
+        else
+        {
+            tileVal = ColourFromBackground(true);
+        }
+        IncreaseClicks(tileVal);
+        return tileVal;
+    }
+
+    void GenerateBackground()
+    {
+        switch (GameController.instance.level)
+        {
+
+           
+            case 3:
+                bgValue = Random.Range(1, 4);
+                if (bgValue == 1)
+                    bgValue = 0;
+                break;
+            case 2:
+                bgValue = Random.Range(2, 4);
+                break;
+            case 1:
+                bgValue = 2;
+                break;
+            default:
+                bgValue = Random.Range(0, 4);
+                break;
+        }
+
+    }
+
+    int ColourFromBackground(bool fifth)
+    {
+        int tileColour = 0;
+
+        if (fifth)
+        {
+            if (bgValue > 1)
+            {
+                float tempCol = Random.Range(0, 5);
+                if (tempCol >= 0 && tempCol < 2)
+                    tileColour = 0;
+                else if (tempCol >= 2 && tempCol < 4)
+                    tileColour = 1;
+                else  tileColour = 4;
+                return tileColour;
+            }
+            else
+            {
+                float tempCol = Random.Range(4, 9);
+                if (tempCol >= 4 && tempCol < 6)
+                    tileColour = 2;
+                else if (tempCol >= 6 && tempCol < 8)
+                    tileColour = 3;
+                else tileColour = 4;
+                return tileColour;
+            }
+
+
+        }
+        else
+        {
+            if (bgValue > 1)
+                return Random.Range(0, 2);
+            else
+                return Random.Range(2, 4);
+        }
+
+    }
+
+    void IncreaseClicks(int tileValue)
+    {
+        if (tileValue == 4)
+        {
+            toClick++;
+            return;
+        }
+        switch (bgValue)
+        {
+            case 0:
+                toClick++;
+                break;
+            case 1: break;
+            case 2:
+                if (tileValue == 0)
+                    toClick++;
+                break;
+            case 3:
+                if (tileValue == 1)
+                    toClick++;
+                break;
+        }
+    }
+
+    public void ManualGeneration(int tileCount, int [] tileColors, int background)
+    {
+        bgValue = background;
+        tilePositions = new int[tileCount];
+
+        PickTilesPosition(tileCount);
+        ManualColourSet(tileColors);
+    }
+
+    void ManualColourSet(int [] tileColors)
+    {
+        for (int k = 0; k < tiles.Length; k++)
+        {
+            bool found = false;
+            tiles[k].Clicked = false;
+            tiles[k].Locked = false;
+
+            for (int t = 0; t < tilePositions.Length; t++)
+            {
+                if (tilePositions[t] == k)
+                {
+                    tiles[k].Value = tileColors[t];
+                    found = true;
+
+                }
+            }
+            if (!found) tiles[k].Value = bgValue;
+            tiles[k].SetColour(bgValue);
+
+        }
+    }
+
+
+   public  int CheckCorrect(RaycastHit2D hit)
+    {
+
+        Tile clickedTile;
+        if (!GameController.instance.pause && !GameController.instance.tutorial)
+        {
+            for (int j = 0; j < tiles.Length; j++)
+            {
+                if (tiles[j].Tile_Reference == hit.transform.gameObject)
+                {
+                    clickedTile = tiles[j];
+
+                    if (!clickedTile.Clicked)
+                    {
+                        if (clickedTile.Value == 4)
+                        {
+                            if (!clickedTile.Locked)
+                            {
+                                GameController.instance.ToClick -= 1;
+                                clickedTile.Clicked = true;
+                                //coin.Play();
+                                clickedTile.Value = bgValue;
+                                clickedTile.SetColour(bgValue);
+                                return 1;
+                            }
+                            { clickedTile.Locked = false; }
+
+                        }
+                        else
+                        {
+                            switch (bgValue)
+                            {
+                                case 0:
+                                    if (!clickedTile.Locked)
+                                    {
+                                        GameController.instance.ToClick -= 1;
+                                        clickedTile.Clicked = true;
+                                      //  coin.Play();
+                                        clickedTile.Value = bgValue;
+                                        clickedTile.SetColour(bgValue);
+                                        return 1;
+                                    }
+                                    { clickedTile.Locked = false; }
+                                    break;
+                                case 1:
+                                    //GameOver(wrong);
+                                    //error.Play();
+                                    return -1;
+                                  //  break;
+                                case 2:
+                                    if (clickedTile.Value == 0)
+                                    {
+
+                                        if (!clickedTile.Locked)
+                                        {
+                                            GameController.instance.ToClick -= 1;
+                                            clickedTile.Clicked = true;
+                                           // coin.Play();
+                                            clickedTile.Value = bgValue;
+                                            clickedTile.SetColour(bgValue);
+                                            return 1;
+                                        }
+                                        { clickedTile.Locked = false; }
+                                        break;
+
+                                    }
+                                    else
+                                    {
+                                        //GameOver(wrong);
+                                        //error.Play();
+                                        return -1;
+                                    }
+
+                                   // break;
+                                case 3:
+                                    if (clickedTile.Value == 1)
+                                    {
+
+                                        if (!clickedTile.Locked)
+                                        {
+                                            GameController.instance.ToClick -= 1;
+                                            clickedTile.Clicked = true;
+                                            //    coin.Play();
+                                            clickedTile.Value = bgValue;
+                                            clickedTile.SetColour(bgValue);
+                                            return 1;
+                                        }
+                                        { clickedTile.Locked = false; }
+                                        break;
+
+                                    }
+                                    else
+                                        {
+                                        //    GameOver(wrong);
+                                        //    error.Play();
+                                        return -1;
+                                    }
+                                   // break;
+
+
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+}
