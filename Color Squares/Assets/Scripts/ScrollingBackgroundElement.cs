a0a6804cd1264185a4b19b15865e5cff7323e4ce
Cristian Gurra <gurra.cristian@gmail.com>
2019-02-11 15:44:44 +0100

    graphics update, improved main menu

---
+++ Color Squares/Assets/Scripts/ScrollingBackgroundElement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackgroundElement : MonoBehaviour {
+
+    public float speed;
+    public Transform endPoint;
+    public Transform startPoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector2(Time.deltaTime * speed, -Time.deltaTime * speed));
+        if(transform.position.x> endPoint.position.x)
+        {
+            transform.position = startPoint.position;
+        }
+	}
+}
